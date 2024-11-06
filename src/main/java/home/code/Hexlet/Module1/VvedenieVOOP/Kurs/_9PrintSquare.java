c1ea41523014b8c4ab69e5de2092d31ba31dece5
CherepovAlex <westler123@yandex.ru>
2024-11-07 00:01:38 +0300

    fix errors

--- src/main/java/home/code/Hexlet/Module1/VvedenieVOOP/Kurs/_9PrintSquare.java
+++ src/main/java/home/code/Hexlet/Module1/VvedenieVOOP/Kurs/_9PrintSquare.java
@@ -1,7 +1,7 @@
 package home.code.Hexlet.Module1.VvedenieVOOP.Kurs;
 class Rectangle extends Exception {
-    private int dlina;
-    private int shirina;
+    private final int dlina;
+    private final int shirina;
 
     Rectangle(int dlina, int shirina) {
         this.dlina = dlina;
@@ -28,7 +28,7 @@ class Rectangle extends Exception {
 
 class App9 {
     // BEGIN (write your solution here)
-    public static void printSquare(Rectangle rectangle) throws Exception {
+    public static void printSquare(Rectangle rectangle) {
         if (rectangle.getDlina() < 0 || rectangle.getShirina() < 0) {
             System.out.println("Не удалось посчитать площадь");
         } else {
@@ -41,14 +41,8 @@ class App9 {
 public class _9PrintSquare {
     public static void main(String[] args) throws Exception {
         var figure1 = new Rectangle(4, 5);
-        var square = figure1.getSquare(); // 20
-
+//        var square = figure1.getSquare(); // 20
         var figure2 = new Rectangle(4, -5); // Тут должно выбрасываться исключение
-//        var square1 = figure2.getSquare();
-
-//        figure1 = new Rectangle(4, 5);
-//        App9.printSquare(figure1); // => 20
-
         figure2 = new Rectangle(3, -2);
         App9.printSquare(figure2); // => Не удалось посчитать площадь
     }
