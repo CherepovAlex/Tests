bfd984f6b16213113d624d0f5af15b8d7da4686a
CherepovAlex <westler123@yandex.ru>
2024-11-07 18:26:17 +0300

    add  na java map

---
+++ src/main/java/home/code/Hexlet/Module2/JavaMaps/_1CatalogGoods.java
@@ -0,0 +1,44 @@
+package home.code.Hexlet.Module2.JavaMaps;
+
+import java.util.Map;
+
+class App1 {
+//    private static Map<String, Double> products = Data.getProducts();
+    private static final Map<String, Double> products = Map.of(
+            "Coffee", 30.0,
+            "Bread", 10.0,
+            "Milk", 20.0
+    );
+
+    public static Double getPriceWithDiscount(Map<String, Double> discounts, String nameGood) {
+
+        if (products.containsKey(nameGood)) {
+            if (discounts.containsKey(nameGood)) {
+                return products.get(nameGood) - products.get(nameGood) * discounts.get(nameGood) / 100;
+            } else {
+                return products.get(nameGood);
+            }
+        } else {
+            return null;
+        }
+    }
+}
+
+public class _1CatalogGoods {
+    public static void main(String[] args) {
+
+        var discounts = Map.of(
+                "Bread", 10.0,
+                "Salmon", 20.0
+        );
+
+        // Скидка на хлеб 10 процентов
+        System.out.println(App1.getPriceWithDiscount(discounts, "Bread")); // 9.0
+
+        // На молоко сегодня скидок нет
+        System.out.println(App1.getPriceWithDiscount(discounts, "Milk")); // 20.0
+
+        System.out.println(App1.getPriceWithDiscount(discounts, "Olives")); // null
+
+    }
+}
