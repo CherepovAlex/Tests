feb30473e29140e864ebac9923b199da6440a672
CherepovAlex <westler123@yandex.ru>
2024-11-11 09:28:18 +0300

    add java function ex6

---
+++ src/main/java/home/code/Hexlet/Module2/JavaFunctions/_6PerformString.java
@@ -0,0 +1,37 @@
+package home.code.Hexlet.Module2.JavaFunctions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+class App6 {
+    public static List<String> map(List<String> list, Function<String, String > fn) {
+        var words = new ArrayList<String>();
+        for (String word : list) {
+            words.add(fn.apply(word));
+        }
+        return words;
+    }
+}
+
+//@FunctionalInterface
+//interface Function {
+//    String test(String str);
+//}
+
+
+public class _6PerformString {
+    public static void main(String[] args) {
+        var words = new ArrayList<String>();
+        words.add("Java");
+        words.add("Python");
+        words.add("PHP");
+
+        var result = App6.map(words, word -> word.toUpperCase());
+
+// Или используя ссылку на метод
+// var result = App.map(words, String::toUpperCase());
+
+        System.out.println(result); // ["JAVA", "PYTHON", "PHP"]
+    }
+}
