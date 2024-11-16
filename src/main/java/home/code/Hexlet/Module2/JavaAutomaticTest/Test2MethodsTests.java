db7d640b30961593d4eea9a6efe208b16981c578
CherepovAlex <westler123@yandex.ru>
2024-11-16 22:38:10 +0300

    add java tests ex 2

---
+++ src/main/java/home/code/Hexlet/Module2/JavaAutomaticTest/Test2MethodsTests.java
@@ -0,0 +1,41 @@
+package home.code.Hexlet.Module2.JavaAutomaticTest;
+
+import java.util.Arrays;
+
+import static home.code.Hexlet.Module2.JavaAutomaticTest.Methods2.take;
+
+public class Test2MethodsTests {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        take(numbers, 1); // [1]
+        take(numbers, 2); // [1, 2]
+        take(numbers, 9); // [1, 2, 3]
+
+        int[] emptyNumbers = {};
+        take(emptyNumbers, 2); // []
+    }
+
+
+    //    @Test
+    public void testTake() {
+        //BEGIN (write your solution here)
+        int[] numbers = {1, 2, 3};
+        int[] numbers1 = {1, 2};
+        assert Arrays.equals(numbers1, take(numbers, 2));
+
+        int[] numbers2 = {1};
+        assert Arrays.equals(numbers2, take(numbers, 1));
+
+        int[] numbers3 = {1, 2, 3};
+        assert Arrays.equals(numbers3, take(numbers, 9));
+
+        int[] emptyNumbers = {};
+        int[] emptyNumbers1 = {};
+        assert Arrays.equals(emptyNumbers1, take(emptyNumbers, 2));
+
+        int[] emptyNumbers2 = {1, 2, 3};
+        int[] emptyNumbers3 = {};
+        assert Arrays.equals(emptyNumbers3, take(emptyNumbers2, 0));
+        //END
+    }
+}
